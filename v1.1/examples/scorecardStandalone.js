c79f212474d1ee72730ff3a7e57ccebd7c2b34ae
abuchholtz <qudosoft-labs@users.noreply.github.com>
2017-08-09 16:31:52 +0200

    Added v1.1 examples

---
+++ v1.1/examples/scorecardStandalone.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var scorecard = new SAMMCharts.Scorecard({
+		element: '#scorecard',
+		scores: {
+			governance: [0.5, 0, 2],
+			construction: [0, 0.5, 0],
+			verification: [0.5, 1.5, 0],
+			deployment: [0, 0.5, 3]
+		}
+	});
+	scorecard.render();
+});
